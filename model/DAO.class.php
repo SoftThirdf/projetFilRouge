bfd082fd49bd93fd31edd74906b44d75fd4df9c6
LucieM1SI <61065820+LucieM1SI@users.noreply.github.com>
2020-03-29 18:11:23 +0200

    Revert "Revert "Merge remote-tracking branch 'origin/branchCyrilV3' into LucieBranche""
    This reverts commit 7574114772b9e18de78067f616809859db0cc8df.

--- model/DAO.class.php
+++ model/DAO.class.php
@@ -34,8 +34,17 @@
 
        //Méthode qui renvoies les informlations lors d'un tournoi et d'une catégorie donnée en paramètre
        //Elle retourne un tableau associatif avec les noms et prénoms des joueurs ainsi que les sets mis ordonné par les numéro de macths
-       function getInfoTypeMatchTournoi($typeTournoi, $categorieTournoi) {
-         $req = "SELECT R.id_Match, J.id_joueur, R.libelle_match, J.Nom_joueur, J.Prenom_joueur, J.Nationalite_joueur, M.Nb_jeu_simple FROM Marque M INNER JOIN rencontre R ON M.id_match = R.id_Match INNER JOIN joueur J ON M.id_joueur = J.id_joueur INNER JOIN tournoi T ON R.id_Tournoi = T.id_Tournoi WHERE T.type_tournoi like '$typeTournoi' AND T.categorie_tournoi like '$categorieTournoi' ORDER BY R.id_Match, J.id_joueur";
+       function getInfoTypeMatchTournoiSimple($typeTournoi) {
+         $req = "SELECT R.id_Match, J.id_joueur, R.libelle_match, J.Nom_joueur, J.Prenom_joueur, J.Nationalite_joueur, B.Nb_jeu FROM balle_set B INNER JOIN rencontre R ON B.id_match = R.id_Match INNER JOIN joueur J ON B.id_joueur = J.id_joueur INNER JOIN tournoi T ON R.id_Tournoi = T.id_Tournoi WHERE T.type_tournoi like '$typeTournoi' AND T.categorie_tournoi like 'Simple' ORDER BY R.id_Match, J.id_joueur";
+         $sth = $this->db->query($req);
+         $res = $sth->fetchAll(PDO::FETCH_ASSOC);
+         return $res;
+
+
+       }
+
+       function getInfoTypeMatchTournoiDouble($typeTournoi) {
+         $req = "SELECT R.id_Match, J.nom_equipe, J.id_joueur, J.nom_joueur, J.prenom_joueur, J.Nationalite_joueur, R.libelle_match, B.Nb_jeu FROM balle_set B INNER JOIN rencontre R ON B.id_match = R.id_Match INNER JOIN joueur J ON B.id_joueur = J.id_joueur INNER JOIN tournoi T ON R.id_Tournoi = T.id_Tournoi WHERE T.type_tournoi like '$typeTournoi' AND T.categorie_tournoi like 'Double'";
          $sth = $this->db->query($req);
          $res = $sth->fetchAll(PDO::FETCH_ASSOC);
          return $res;
