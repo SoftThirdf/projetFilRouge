7a87e72e787dbbd000734dec8a407e93e9aadd95
SoftThirdf <cyril.ro38@gmail.com>
2020-03-30 14:42:24 +0200

    Fix insert

--- model/DAO.class.php
+++ model/DAO.class.php
@@ -33,7 +33,7 @@
 
 
        //Méthode qui renvoies les informlations lors d'un tournoi et d'une catégorie donnée en paramètre
-       //Elle retourne un tableau associatif avec les noms et prénoms des joueurs ainsi que les sets mis ordonné par les numéro de macths
+       //Elle retourne un tableau associatif avec les noms et prénoms des joueurs ainsi que les sets mis ordonné par les numéro de matchs
        function getInfoTypeMatchTournoiSimple($typeTournoi) {
          $req = "SELECT R.id_Match, J.id_joueur, R.libelle_match, J.Nom_joueur, J.Prenom_joueur, J.Nationalite_joueur, B.Nb_jeu FROM balle_set B INNER JOIN rencontre R ON B.id_match = R.id_Match INNER JOIN joueur J ON B.id_joueur = J.id_joueur INNER JOIN tournoi T ON R.id_Tournoi = T.id_Tournoi WHERE T.type_tournoi like '$typeTournoi' AND T.categorie_tournoi like 'Simple' ORDER BY R.id_Match, J.id_joueur";
          $sth = $this->db->query($req);
@@ -42,9 +42,10 @@
 
 
        }
-
+       //Méthode qui renvoies les informlations lors d'un tournoi et d'une catégorie donnée en paramètre
+       //Elle retourne un tableau associatif avec les noms et prénoms des joueurs ainsi que les sets mis ordonné par les numéro de matchs
        function getInfoTypeMatchTournoiDouble($typeTournoi) {
-         $req = "SELECT R.id_Match, J.nom_equipe, J.id_joueur, J.nom_joueur, J.prenom_joueur, J.Nationalite_joueur, R.libelle_match, B.Nb_jeu FROM balle_set B INNER JOIN rencontre R ON B.id_match = R.id_Match INNER JOIN joueur J ON B.id_joueur = J.id_joueur INNER JOIN tournoi T ON R.id_Tournoi = T.id_Tournoi WHERE T.type_tournoi like '$typeTournoi' AND T.categorie_tournoi like 'Double'";
+         $req = "SELECT R.id_Match, J.nom_equipe, J.id_joueur, J.nom_joueur, J.prenom_joueur, J.Nationalite_joueur, R.libelle_match, B.Nb_jeu FROM balle_set B INNER JOIN rencontre R ON B.id_match = R.id_Match INNER JOIN joueur J ON B.id_joueur = J.id_joueur INNER JOIN tournoi T ON R.id_Tournoi = T.id_Tournoi WHERE T.type_tournoi like '$typeTournoi' AND T.categorie_tournoi like 'Double' GROUP BY R.id_Match, J.id_joueur";
          $sth = $this->db->query($req);
          $res = $sth->fetchAll(PDO::FETCH_ASSOC);
          return $res;
--- model/request.sql
+++ model/request.sql
@@ -1,5 +1,5 @@
 -- Les noms et prénoms des joueurs ainsi que les sets mis ordonné par les numéro de macths lors d'un tournoi qualificatif simple
-SELECT R.id_Match, J.Nom_joueur, J.Prenom_joueur, J.Nationalite_joueur, B.Nb_jeu
+SELECT R.id_Match, J.Nom_joueur, J.Prenom_joueur, J.Nationalite_joueur, B.Nb_jeu, R.libelle_match
 FROM Balle_set B
 INNER JOIN rencontre R ON B.id_match = R.id_Match
 INNER JOIN joueur J ON B.id_joueur = J.id_joueur
@@ -9,11 +9,10 @@ AND T.categorie_tournoi like 'Simple'
 ORDER BY R.id_Match;
 
 -- Les noms des équipes ainsi que les sets mis ordonné par les numéro de macths lors d'un tournoi qualificatif double
-SELECT R.id_Match,E.id_equipe, E.nom_equipe,J.id_joueur, J.nom_joueur, J.prenom_joueur, J.Nationalite_joueur, R.libelle_match, M.Nb_jeu_simple
-FROM Marque M
-INNER JOIN rencontre R ON M.id_match = R.id_Match
-INNER JOIN joueur J ON M.id_joueur = J.id_joueur
-INNER JOIN equipe E on E.id_equipe = J.id_equipe
-INNER JOIN tournoi T ON R.id_Tournoi = T.id_Tournoi
-WHERE T.type_tournoi like 'Eliminatoire'
+SELECT R.id_Match, J.nom_equipe, J.id_joueur, J.nom_joueur, J.prenom_joueur, J.Nationalite_joueur, R.libelle_match, B.Nb_jeu
+FROM balle_set B
+INNER JOIN rencontre R ON B.id_match = R.id_Match
+INNER JOIN joueur J ON B.id_joueur = J.id_joueur
+INNER JOIN tournoi T ON R.id_Tournoi = T.id_Tournoi WHERE T.type_tournoi like 'Eliminatoire'
 AND T.categorie_tournoi like 'Double'
+GROUP BY R.id_Match, J.id_joueur
