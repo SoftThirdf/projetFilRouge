684c1f0119e9e175087cd794b3c1b45f1188411e
SoftThirdf <cyril.ro38@gmail.com>
2020-04-25 13:14:00 +0200

    Modif css StandDedicasse

--- view/stands/StandDedicaceVIP.php
+++ view/stands/StandDedicaceVIP.php
@@ -8,16 +8,35 @@
     die ("Erreur : " . $e -> getMessage());
   }
 
-  $reponse = $bdd -> query ("SELECT V.id_VIP, V.nom_VIP, V.prenom_VIP, P.popularite_VIP, V.type_VIP, V.nationalite_VIP, V.nb_grands_chelems, V.classement_ATP_simple, V.classement_ATP_double FROM vip V, popularite P WHERE V.id_popularite = P.id_popularite AND V.id_VIP = 45 UNION SELECT V.id_VIP, V.nom_VIP, V.prenom_VIP, P.popularite_VIP, V.type_VIP, V.nationalite_VIP, V.nb_grands_chelems, V.classement_ATP_simple, V.classement_ATP_double FROM vip V, popularite P WHERE V.id_popularite = P.id_popularite AND V.id_VIP = 43 UNION SELECT V.id_VIP, V.nom_VIP, V.prenom_VIP, P.popularite_VIP, V.type_VIP, V.nationalite_VIP, V.nb_grands_chelems, V.classement_ATP_simple, V.classement_ATP_double FROM vip V, popularite P WHERE V.id_popularite = P.id_popularite AND V.id_VIP = 34");
+$reponse = $bdd -> query ("SELECT V.id_VIP, V.nom_VIP, V.prenom_VIP, P.popularite_VIP, V.type_VIP, V.nationalite_VIP, V.nb_grands_chelems, V.classement_ATP_simple, V.classement_ATP_double
+FROM vip V, popularite P
+WHERE V.id_popularite = P.id_popularite
+AND V.nom_VIP LIKE \"François\"
+AND V.prenom_VIP LIKE \"Maxime\"
+
+UNION
+SELECT V.id_VIP, V.nom_VIP, V.prenom_VIP, P.popularite_VIP, V.type_VIP, V.nationalite_VIP, V.nb_grands_chelems, V.classement_ATP_simple, V.classement_ATP_double
+FROM vip V, popularite P
+WHERE V.id_popularite = P.id_popularite
+AND V.nom_VIP LIKE \"Michel\"
+AND V.prenom_VIP LIKE \"Emeline\"
+
+UNION
+SELECT V.id_VIP, V.nom_VIP, V.prenom_VIP, P.popularite_VIP, V.type_VIP, V.nationalite_VIP, V.nb_grands_chelems, V.classement_ATP_simple, V.classement_ATP_double
+FROM vip V, popularite P
+WHERE V.id_popularite = P.id_popularite
+AND V.nom_VIP LIKE \"Gökdere\"
+AND V.prenom_VIP LIKE \"Özge\"
+");
 
 ?>
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="../style/VIP.css">
   <link rel="stylesheet" href="../style/index.css">
-  <link rel="stylesheet" href="../style/StandMenu.css">
+  <link rel="stylesheet" href="../style/VIP.css">
+  <link rel="stylesheet" href="../style/StandDedicaceVIP.css">
   <title> Dédicaces </title>
 </head>
 
@@ -35,62 +54,67 @@
   }
   ?>
 
+  <div id="conteneurPage">
+    <div id="actualitevip">
+      <h2>Venez rencontrer nos VIP pour une séance de dédicace !</h2>
+      <hr class="sousH2">
+    </div>
 
-<div id="actualitevip">
- <h2>Venez rencontrer nos VIP pour une séance de dédicace !</h2>
- <hr class="sousH2">
-</div>
+    <div id="conteneurProfils">
 
-<?php
+      <?php
 
-  while ($donnees = $reponse -> fetch()) {
+        while ($donnees = $reponse -> fetch()) {
 
-  $nomPays = strtolower($donnees['nationalite_VIP']);
-  $lienDrap = "\"img/pays/$nomPays.png\"";
+        $nomPays = strtolower($donnees['nationalite_VIP']);
+        $lienDrap = "../img/pays/$nomPays.png";
 
-  $id_vip = $donnees['id_VIP'];
-  $nom_vip = $donnees['nom_VIP'];
-  $prenom_vip = $donnees['prenom_VIP'];
-  $lienphoto = "../img/imgVIP/imgProfilVIP/$id_vip-$nom_vip-$prenom_vip.jpg";
+        $id_vip = $donnees['id_VIP'];
+        $nom_vip = $donnees['nom_VIP'];
+        $prenom_vip = $donnees['prenom_VIP'];
+        $lienphoto = "../img/imgVIP/imgProfilVIP/$nom_vip-$prenom_vip.jpg";
 
- ?>
+      ?>
 
- <div id="profilVIP">
-   <?php
-   echo "<img src=$lienphoto alt=\"vip\" class=\"photoVIP\">";
-   ?>
-   <h3>
-     <?php
-       echo $prenom_vip . " " . $nom_vip . " " ."<img src=$lienDrap alt=\"$nomPays\" class='imgDrap'><br>";
-     ?>
-   </h3>
-   <hr class="sousH3">
+      <div class="profilVIP">
+        <?php
+          echo "<img src=$lienphoto alt=\"vip\" class=\"photoVIP\">";
+        ?>
+        <h3>
+        <?php
+          echo $prenom_vip . " " . $nom_vip . " " ."<img src=$lienDrap alt=\"$nomPays\" class='imgDrap'><br>";
+        ?>
+        </h3>
+        <hr class="sousH3">
 
-   <a href="https://www.instagram.com" target="_blank"><img src="../img/imgVIP/1024px-Instagram_simple_icon.svg.png" class="imgreseaux"></a>
-   <a href="https://www.facebook.com" target="_blank"><img src="../img/imgVIP/Facebook-Logo.png" class="imgreseaux"></a>
-   <a href="https://twitter.com" target="_blank"><img src="../img/imgVIP/Twitter_Bird.svg.png" class="imgreseaux"></a>
+        <a href="https://www.instagram.com" target="_blank"><img src="../img/imgVIP/insta.png" class="rs"></a>
+        <a href="https://www.facebook.com" target="_blank"><img src="../img/imgVIP/fb.png" class="rs"></a>
+        <a href="https://twitter.com" target="_blank"><img src="../img/imgVIP/twitter.png" class="rs"></a>
 
-   <h3><?php echo $donnees['popularite_VIP']; ?></h3>
-   Type de VIP : <?php echo $donnees['type_VIP']; ?> <br>
+        <h3>
+          <?php echo $donnees['popularite_VIP']; ?>
+        </h3>
+        Type de VIP :
+        <?php echo $donnees['type_VIP']; ?> <br>
 
-   <?php if ($donnees['nb_grands_chelems'] != 0){ echo "Nombre de Grands chelems : ". $donnees['nb_grands_chelems'];} ?> <br>
-   <?php if ($donnees['classement_ATP_simple'] != 0){ echo "Classement ATP Simple : " . $donnees['classement_ATP_simple'];} ?> <br>
-   <?php if ($donnees['classement_ATP_double'] != 0){ echo "Classement ATP Double: " . $donnees['classement_ATP_double'];} ?> <br>
- </div>
+        <?php if ($donnees['nb_grands_chelems'] != 0){ echo "Nombre de Grands chelems : ". $donnees['nb_grands_chelems'];} ?> <br>
+        <?php if ($donnees['classement_ATP_simple'] != 0){ echo "Classement ATP Simple : " . $donnees['classement_ATP_simple'];} ?> <br>
+        <?php if ($donnees['classement_ATP_double'] != 0){ echo "Classement ATP Double: " . $donnees['classement_ATP_double'];} ?> <br>
+      </div>
 
-<?php
+      <?php
 }
 ?>
-
-<div id="actualitevip">
- <h3>Dédicaces</h3>
- <hr class="sousH3">
- <p>Maxime François tenant du titre de l’Open Sopra Steria, sa compagne Emeline Michel demi-finaliste du grand chelem de Roland Garos sont venus en tant que grands invités à l’Open Sopra Steria !
-   Accompagnés par l’excentrique Özge Gökdere, journaliste talentueuse aussi bien connue pour ses qualités d’investigation que son caractère surprenant, qui mène un reportage sur la vie des stars féminines de l’Open !
-   Ces trois invités prestigieux offriront aux fans une session de dédicace chaque matin de 10h à 12h afin de pouvoir côtoyer leurs fans !
-   C’est un moment unique de passer un moment spécial avec ces célébrités, ne ratez pas cette occasion ! </p>
-</div>
-
+    </div>
+    <div id="actualitevip">
+      <h3>Dédicaces</h3>
+      <hr class="sousH3">
+      <p>Maxime François tenant du titre de l’Open Sopra Steria, sa compagne Emeline Michel demi-finaliste du grand chelem de Roland Garos sont venus en tant que grands invités à l’Open Sopra Steria !
+        Accompagnés par l’excentrique Özge Gökdere, journaliste talentueuse aussi bien connue pour ses qualités d’investigation que son caractère surprenant, qui mène un reportage sur la vie des stars féminines de l’Open !
+        Ces trois invités prestigieux offriront aux fans une session de dédicace chaque matin de 10h à 12h afin de pouvoir côtoyer leurs fans !
+        C’est un moment unique de passer un moment spécial avec ces célébrités, ne ratez pas cette occasion ! </p>
+    </div>
+  </div>
 </body>
 
 <footer>
@@ -101,7 +125,7 @@
     <ol class="navigationFooterOl">
       <li class="marginBottom10"><a href="../index.php" class="linkWhite">Accueil</a></li>
       <li class="marginBottom10"><a href="../../controler/tournoisSimQuaControler.php" class="linkWhite">Tableaux des tournois</a></li>
-	  <li class="marginBottom10"><a href="StandMenu.php" class="linkWhite">Stands de l'open</a></li>
+      <li class="marginBottom10"><a href="StandMenu.php" class="linkWhite">Stands de l'open</a></li>
       <li class="marginBottom10"><a href="../../controler/listeVIPControler.php" class="linkWhite">VIP</a></li>
 
     </ol>
